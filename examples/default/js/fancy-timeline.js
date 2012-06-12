7d594ec94a0062fcde30268830669bcd0d8f5fca
juanma <juanma.aguero@gmail.com>
2012-06-11 21:57:01 -0300

    License update and cleanup directories

--- examples/default/js/fancy-timeline.js
+++ examples/default/js/fancy-timeline.js
@@ -1,9 +1,24 @@
-/*
- * Fancy TimeLine
- * Author: Juan Manuel Aguero, http://juanmaaguero.com.ar
- * Doc: 
- * 
- */
+/*!
+ *	Fancy-TimeLine V0.1.0
+ *	
+ *	Copyright (C) 2012 Juan Manuel Aguero (http://juanmaaguero.com.ar/sobre-mi)
+ *	
+ *	This file is part of Fancy-TimeLine.
+ *	
+ *	Fancy-TimeLine is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	Fancy-TimeLine is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with Fancy-TimeLine.  If not, see <http://www.gnu.org/licenses/>.
+ *	
+***/
 
 actualPos = 0;
 (function($) {  
